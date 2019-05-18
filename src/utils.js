9a4ba1f0e61c7d3439f45fc1548388cac096b022
neotmons <neotmons@gmail.com>
2019-05-18 11:32:05 +0900

    #3.5 Passport JWT part TWO

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,7 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { adjectives, nouns } from "./words";
 import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-sendgrid-transport";
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -24,10 +25,13 @@ const sendMail = email => {
 
 export const sendSecretMail = (adress, secret) => {
   const email = {
-    from: "meringuewhy@gmail.com",
+    from: "neotmons@gmail.com",
     to: adress,
     subject: "Login Secret for Prismagram",
-    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`
+    html: `Hello! Your login secret is <b>${secret}</b>.<br/>Copy paste on the app/website to log in`
   };
   return sendMail(email);
 };
+
+console.log('pre generate tocket')
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
