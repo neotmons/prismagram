87f1c02846fff643d09377357093a61902209375
neotmons <neotmons@gmail.com>
2019-05-18 23:12:31 +0900

    3.8 toggleLike and addComment Resolver

---
+++ src/api/Comment/addComment/addComment.js
@@ -0,0 +1,26 @@
+import { isAutehnticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation: {
+        addComment: async(_, args, {request}) => {
+            isAutehnticated(request);
+            const {text, postId} = args;
+            const {user} = request;
+            const comment = await prisma.createComment({
+                user: {
+                    connect: {
+                        id: user.id
+                    }
+                },
+                post: {
+                    connect: {
+                        id: postId
+                    }
+                },
+                text              
+            })
+            return comment;
+        }
+    }
+}
--- src/api/Post/toggleLike/toggleLike.js
+++ src/api/Post/toggleLike/toggleLike.js
@@ -9,26 +9,28 @@ export default {
             const { postId } = args;
             const { user } = request;
 
+            const filterOptions = {
+                AND: [
+                    {
+                        user:{
+                            id: user.id
+                        }
+                    },
+                    {
+                        post:{
+                            id: postId
+                        }
+                    }
+                ]
+            };
+
             try {
 
-                const existingLike = await prisma.$exists.like({
-                    AND: [
-                        {
-                            user:{
-                                id: user.id
-                            }
-                        },
-                        {
-                            post:{
-                                id: postId
-                            }
-                        }
-                    ]
-                });
+                const existingLike = await prisma.$exists.like(filterOptions);
 
                 
                 if(existingLike){
-                    // To Do
+                    await prisma.deleteManyLikes(filterOptions);
                 } else {
                     await prisma.createLike({
                         user: {
