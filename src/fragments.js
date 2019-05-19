16a36eebb6330c03aa22816c1951429e69245fe8
neotmons <neotmons@gmail.com>
2019-05-19 11:17:53 +0900

    #3.13 See Full Posts

--- src/fragments.js
+++ src/fragments.js
@@ -1,14 +1,25 @@
 export const USER_FRAGMENT = `
-fragment UserParts on User{
-    id,
-    name,
-    email,
-    firstName
-    lastName
-    bio
-    posts {
-        id
-        caption
+    fragment UserParts on User{
+        id,
+        name,
+        email,
+        firstName
+        lastName
+        bio
+        posts {
+            id
+            caption
+        }
     }
-}
+`;
+
+
+export const COMMENT_FRAGMENT = `
+    fragment CommentParts on Coment{
+        id,
+        text,
+        user {
+            name
+        }
+    }
 `;
