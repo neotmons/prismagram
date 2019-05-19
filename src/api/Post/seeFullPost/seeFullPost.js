c616810c910c4ae8f5b2cd43e85e827f62209354
neotmons <neotmons@gmail.com>
2019-05-19 16:31:46 +0900

    #3.18 upload Resolver

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -6,18 +6,25 @@ export default {
         seeFullPost: async(_, args) => {
             const {id} = args;
             const post = await prisma.post({id});
-            const comments = await prisma.post({id}).comments().$fragment(COMMENT_FRAGMENT);
+            const comments = await prisma
+                .post({id})
+                .comments()
+                .$fragment(COMMENT_FRAGMENT);
             const likeCount = await prisma
-            .likesConnection({
-                where: { post: { id }}
-            })
-            .aggregate()
-            .count();
-
+                .likesConnection({
+                    where: { post: { id }}
+                })
+                .aggregate()
+                .count();
+            const files = await prisma.post({id}).files();
+            const user = await prisma.post({id}).user();
+            
             return {
                 post, 
                 comments, 
-                likeCount
+                likeCount,
+                files,
+                user
             };
         }
     }
